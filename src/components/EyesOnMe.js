96cd0d4839baad75954fe19b81e190bcd3803d46
maryanne-rose <rosemary.njeri@student.moringaschool.com>
2022-07-16 11:38:40 +0300

    passing events

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+const EyesOnMe = () => {
+    const handle = () =>{
+        console.log("Good!")
+    }
+    const blur = () => {
+        console.log("Hey! Eyes on me!")
+    }
+    return (
+        <button onFocus={handle} onBlur={blur}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,19 @@
+
 // Code Keypad Component Here
+function Keypad() {
+
+    const change = () => {
+        console.log('Entering password...')
+    }    
+
+    return (
+        <div>
+            <input 
+                type="password"
+                onChange={change}
+            />
+        </div>
+    );
+}
+
+export default Keypad;
